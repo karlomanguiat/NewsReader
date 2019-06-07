c56661f884f54a231de9667f98a6d2d652c9bd38
karlomanguiat <gdmanguiat@up.edu.ph>
2019-06-07 13:23:30 +0800

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Glenn Karlo Manguiat on 07/06/2019.
+//  Copyright © 2019 Glenn Karlo Manguiat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
