6f3a93d8c1e219020f79760138a0bb2c9802ef39
karlomanguiat <gdmanguiat@up.edu.ph>
2019-06-13 15:27:19 +0800

    Filter update

--- News/FilterViewController.swift
+++ News/FilterViewController.swift
@@ -16,9 +16,11 @@ class FilterViewController: UIViewController {
     @IBOutlet weak var filterButton: UIButton!
     @IBOutlet weak var categoryButton: UIButton!
     @IBOutlet weak var countryButton: UIButton!
+    @IBOutlet weak var keywordSearch: UITextField!
     
     let CountryDropDown = DropDown()
     let CategoryDropDown = DropDown()
+    
     
     let CountryDictionary: [String:String] = ["United Arab Emirates" : "ae",
                                                 "Argentina" : "ar",
@@ -81,7 +83,12 @@ class FilterViewController: UIViewController {
                                                "Health": "health",
                                                "Science" : "science",
                                                "Sports": "sports",
-                                               "Tech": "technology" ]
+                                               "Tech": "technology",
+                                               "N/A" : "none"]
+    
+    var keyword = ""
+    var selectedCountry = ""
+    var selectedCategory = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -91,12 +98,27 @@ class FilterViewController: UIViewController {
         CountryDropDown.selectionAction = { (index: Int, item: String) in
             print("Country item: \(item) at index: \(index)")
             self.countryButton.setTitle(item, for: .normal)
+            self.selectedCountry = item
         }
         
         CategoryDropDown.selectionAction = { (index: Int, item: String) in
             print("Category item: \(item) at index: \(index)")
             self.categoryButton.setTitle(item, for: .normal)
+            self.selectedCategory = item
         }
+        
+        //getnewURL()
+    }
+    
+    func initDropdowns() {
+        CountryDropDown.anchorView = countryDD
+        CountryDropDown.dropDownHeight = 300
+        CountryDropDown.dataSource = CountryDictionary.keys.sorted()
+        
+        CategoryDropDown.anchorView = categoryDD
+        CategoryDropDown.dropDownHeight = 300
+        CategoryDropDown.dataSource = CategoryDictionary.keys.sorted()
+        
     }
     
     func initFilter() {
@@ -111,15 +133,43 @@ class FilterViewController: UIViewController {
         CategoryDropDown.show()
     }
     
+    @IBAction func filterResults(_ sender: Any) {
+        self.keyword = keywordSearch.text!
+        
+        let old_url = getnewURL()
+        print(old_url)
+    }
     
-    func initDropdowns() {
-        CountryDropDown.anchorView = countryDD // UIView or UIBarButtonItem
-        CountryDropDown.dropDownHeight = 300
-        CountryDropDown.dataSource = CountryDictionary.keys.sorted()
+    func getnewURL() -> String {
+        var newURL = ""
+        var country = ""
+        var category = ""
         
-        CategoryDropDown.anchorView = categoryDD
-        CategoryDropDown.dropDownHeight = 300
-        CategoryDropDown.dataSource = CategoryDictionary.keys.sorted()
-        
+        if selectedCategory == "" && selectedCountry == "" {
+            //print(self.keyword)
+            newURL = "https://newsapi.org/v2/top-headlines?q=\(self.keyword)&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+        } else if self.keyword == "" && selectedCategory == "" {
+            country = CountryDictionary[self.selectedCountry]!
+            newURL = "https://newsapi.org/v2/top-headlines?country=\(country)&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+        } else if self.keyword == "" && selectedCategory == "" {
+            category = CategoryDictionary[self.selectedCategory]!
+            newURL = "https://newsapi.org/v2/top-headlines?category=\(category)&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+        } else if selectedCategory == "" {
+            country = CountryDictionary[self.selectedCountry]!
+            newURL = "https://newsapi.org/v2/top-headlines?q=\(self.keyword)&country=\(country)&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+        } else if selectedCountry == "" {
+            category = CategoryDictionary[self.selectedCategory]!
+            newURL = "https://newsapi.org/v2/top-headlines?q=\(self.keyword)&category=\(category)&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+        } else if self.keyword == "" {
+            country = CountryDictionary[self.selectedCountry]!
+            category = CategoryDictionary[self.selectedCategory]!
+            newURL = "https://newsapi.org/v2/top-headlines?country=\(country)&category=\(category)&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+        } else {
+            country = CountryDictionary[self.selectedCountry]!
+            category = CategoryDictionary[self.selectedCategory]!
+            newURL = "https://newsapi.org/v2/top-headlines?q=\(self.keyword)&country=\(country)&category=\(category)&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+        }
+       
+        return newURL
     }
 }
