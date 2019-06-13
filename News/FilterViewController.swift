bec49ad95af139b45542aea395c0fde2d2966487
karlomanguiat <gdmanguiat@up.edu.ph>
2019-06-13 13:30:11 +0800

    Filter

---
+++ News/FilterViewController.swift
@@ -0,0 +1,125 @@
+//
+//  FilterViewController.swift
+//  News
+//
+//  Created by Glenn Karlo Manguiat on 12/06/2019.
+//  Copyright © 2019 Glenn Karlo Manguiat. All rights reserved.
+//
+
+import UIKit
+import DropDown
+
+class FilterViewController: UIViewController {
+    
+    @IBOutlet weak var countryDD: UIView!
+    @IBOutlet weak var categoryDD: UIView!
+    @IBOutlet weak var filterButton: UIButton!
+    @IBOutlet weak var categoryButton: UIButton!
+    @IBOutlet weak var countryButton: UIButton!
+    
+    let CountryDropDown = DropDown()
+    let CategoryDropDown = DropDown()
+    
+    let CountryDictionary: [String:String] = ["United Arab Emirates" : "ae",
+                                                "Argentina" : "ar",
+                                                "Austria" : "at",
+                                                "Australia" : "au",
+                                                "Belgium" : "be",
+                                                "Bulgaria" : "bg",
+                                                "Brazil" : "br",
+                                                "Canada" : "ca",
+                                                "Switzerland" : "ch",
+                                                "China" : "cn",
+                                                "Cuba" : "cu",
+                                                "Colombia" : "co",
+                                                "Czech Republic" : "cz",
+                                                "Germany" : "de",
+                                                "Egypt" : "eg",
+                                                "France" : "fr",
+                                                "United Kingdom" : "gb",
+                                                "Greece" : "gr",
+                                                "Hongkong" : "hk",
+                                                "Hungary" : "hu",
+                                                "Indonesia" : "id",
+                                                "Ireland" : "ie",
+                                                "Israel" : "il",
+                                                "India" : "in",
+                                                "Italy" : "it",
+                                                "Japan" : "jp",
+                                                "Republic of Korea" : "kr",
+                                                "Lithuania" : "lt",
+                                                "Latvia" : "lv",
+                                                "Morocco" : "ma",
+                                                "Mexico" : "mx",
+                                                "Malaysia": "my",
+                                                "Nigeria" : "ng",
+                                                "Netherlands" : "nl",
+                                                "Norway" : "no",
+                                                "New Zealand" : "nz",
+                                                "Philippines" : "ph",
+                                                "Poland" : "pl",
+                                                "Portugal" : "pt",
+                                                "Romania" : "ro",
+                                                "Serbia" : "rs",
+                                                "Russia" : "ru",
+                                                "Saudi Arabia" : "sa",
+                                                "Sweden" : "se",
+                                                "Singapore" : "sg",
+                                                "Slovenia" : "si",
+                                                "Slovakia" : "sk",
+                                                "Thailand" : "th",
+                                                "Turkey" : "tr",
+                                                "Taiwan" : "tw",
+                                                "Ukraine" : "ua",
+                                                "United States" : "us",
+                                                "Venezuela" : "ve",
+                                                "South Africa" : "za" ]
+    
+    let CategoryDictionary: [String:String] = ["Business": "business",
+                                               "Entertainment": "entertainment",
+                                               "General": "general",
+                                               "Health": "health",
+                                               "Science" : "science",
+                                               "Sports": "sports",
+                                               "Tech": "technology" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initDropdowns()
+        initFilter()
+        
+        CountryDropDown.selectionAction = { (index: Int, item: String) in
+            print("Country item: \(item) at index: \(index)")
+            self.countryButton.setTitle(item, for: .normal)
+        }
+        
+        CategoryDropDown.selectionAction = { (index: Int, item: String) in
+            print("Category item: \(item) at index: \(index)")
+            self.categoryButton.setTitle(item, for: .normal)
+        }
+    }
+    
+    func initFilter() {
+        filterButton.layer.cornerRadius = 4
+    }
+    
+    @IBAction func openCountryDrop(_ sender: Any) {
+        CountryDropDown.show()
+    }
+    
+    @IBAction func openCategoryDrop(_ sender: Any) {
+        CategoryDropDown.show()
+    }
+    
+    
+    func initDropdowns() {
+        CountryDropDown.anchorView = countryDD // UIView or UIBarButtonItem
+        CountryDropDown.dropDownHeight = 300
+        CountryDropDown.dataSource = CountryDictionary.keys.sorted()
+        
+        CategoryDropDown.anchorView = categoryDD
+        CategoryDropDown.dropDownHeight = 300
+        CategoryDropDown.dataSource = CategoryDictionary.keys.sorted()
+        
+    }
+}
---
+++ News/ViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Glenn Karlo Manguiat on 07/06/2019.
+//  Copyright © 2019 Glenn Karlo Manguiat. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var newstableView: UITableView!
+    
+ //   let resourceURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=aIqfQYhGbaAFb8ZKGXe4AzG7jWTQkzn5"
+    let newsURL = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=daf873f4f4f047639bb2cfe67f90d1c9"
+    var headlines = [String]()
+    var abstracts = [String]()
+    var images = [String]()
+    var sources = [String]()
+    var authors = [String]()
+    var urls = [URL]()
+    var publisheddates = [String]()
+    var content = [String]()
+    
+    var headline_name = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getJSON()
+    }
+    
+    func getJSON() {
+        Alamofire.request(newsURL).responseJSON { response in
+            if response.result.value != nil {
+                let json = JSON(response.result.value!)
+                let results = json["articles"].arrayValue
+                
+                for res in results {
+                    let headlines =  res["title"].stringValue
+                    let abstracts =  res["description"].stringValue
+                    let images = res["urlToImage"].stringValue
+                    let urls =  res["url"].url
+                    var dates = res["publishedAt"].stringValue
+                    let authors = res["author"].stringValue
+                    let sources = res["source"]["name"].stringValue
+                    let content = res["content"].stringValue
+                    
+                    //print(dates)
+                    let dateFormatterGet = DateFormatter()
+                    dates = dates.replacingOccurrences(of: "\\.\\d+", with: "", options: .regularExpression)
+                    // dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+                    dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+                    let dateFormatterPrint = DateFormatter()
+                    dateFormatterPrint.dateFormat = "MMM dd, yyyy - h:mm a"
+
+                    
+                    if let date = dateFormatterGet.date(from: dates) {
+                        //print(dateFormatterPrint.string(from: date))
+                        self.publisheddates.append(dateFormatterPrint.string(from: date))
+                    } else {
+                        print("There was an error decoding the string")
+                    }
+                    
+                    self.headlines.append(headlines)
+                    self.abstracts.append(abstracts)
+                    self.images.append(images)
+                    self.urls.append(urls!)
+                    self.sources.append(sources)
+                    self.authors.append(authors)
+                    self.content.append(content)
+                    
+                }
+                
+                DispatchQueue.main.async {
+                    self.newstableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return headlines.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath) as! ViewControllerTableViewCell
+        
+        cell.headerText.text = self.headlines[indexPath.row]
+        cell.detailsText.text = self.abstracts[indexPath.row]
+        cell.dateLabel.text = self.publisheddates[indexPath.row]
+        cell.articleImage.loadImageUsingCacheWithUrlString(self.images[indexPath.row])
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "transfer", sender: self)
+        
+    }
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let indexPath = newstableView.indexPathForSelectedRow {
+            let selectedRow = indexPath.row
+            if let destination = segue.destination as? DetailsViewController {
+                destination.temp_headline = self.headlines[selectedRow]
+                destination.temp_imageURL = self.images[selectedRow]
+                destination.temp_source = self.sources[selectedRow]
+                destination.temp_url = self.urls[selectedRow]
+                destination.temp_date = self.publisheddates[selectedRow]
+                destination.temp_content = self.content[selectedRow]
+                destination.temp_author = self.authors[selectedRow]
+            }
+        }
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+} 
+
